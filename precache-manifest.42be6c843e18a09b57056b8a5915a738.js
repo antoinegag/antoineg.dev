2792bff8388f80db949e08740c82b53f78e26c90
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-12-25 17:01:18 -0500

    Updates

---
+++ precache-manifest.42be6c843e18a09b57056b8a5915a738.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98233ab687e2755bf694afc3273fcbc3",
+    "url": "/antoineg.dev/index.html"
+  },
+  {
+    "revision": "55d9d541e7f870805c74",
+    "url": "/antoineg.dev/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6280668f938940dae4fd",
+    "url": "/antoineg.dev/static/js/2.570f2565.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/antoineg.dev/static/js/2.570f2565.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55d9d541e7f870805c74",
+    "url": "/antoineg.dev/static/js/main.95f020d9.chunk.js"
+  },
+  {
+    "revision": "467f68eee9c816c18f87",
+    "url": "/antoineg.dev/static/js/runtime-main.42229a7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/antoineg.dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
