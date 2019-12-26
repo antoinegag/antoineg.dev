2fdbb3727c0e42042ab7064829de45483a8e54ec
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-12-25 22:37:12 -0500

    Switch to hash router and other fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Projects from "./pages/Projects";
 import Home from "./pages/Home";
 
