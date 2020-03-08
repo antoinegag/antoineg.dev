fd6c732b9ab6b405608ffaabde73dc0a89001675
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-03-08 01:58:57 -0500

    Fix background

--- src/pages/Hello/index.js
+++ src/pages/Hello/index.js
@@ -6,13 +6,15 @@ import { Pro } from "./Pro";
 
 export const Hello = () => {
   return (
-    <div className="lg:w-2/6 lg:mx-auto px-10 hello-bg min-h-full">
-      <Header className="py-10" />
-      <VSCO />
-      <hr className="my-5" />
-      <Instagram />
-      <hr className="my-5" />
-      <Pro />
+    <div className="hello-bg min-h-full min-w-full">
+      <div className="lg:w-2/6 lg:mx-auto px-10">
+        <Header className="py-10" />
+        <VSCO />
+        <hr className="my-5" />
+        <Instagram />
+        <hr className="my-5" />
+        <Pro />
+      </div>
     </div>
   );
 };
