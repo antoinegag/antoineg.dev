b7f106bdc72c01a3eff05e2273004087593aa27f
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-05-07 20:36:53 -0400

    Fix some stuff, add internship stuff, stuff

---
+++ src/pages/Home/Contact.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { useTranslation } from "react-i18next";
+
+export default function Contact() {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <div className="text-center py-10">
+        <h2 className="py-2">{t("contact.title")}.</h2>
+        <p className="text-lg">{t("contact.description")}</p>
+      </div>
+      <div className="text-center text-xl">
+        <div className="my-2 flex items-center justify-center">
+          <a
+            href="mailto:antoine.gagnon5@usherbrooke.ca"
+            className="flex flex-wrap justify-center items-center"
+            target="_top"
+          >
+            <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
+            <div className="flex-grow-0">antoine.gagnon5</div>
+            <div className="flex-grow-0">@usherbrooke.ca</div>
+          </a>
+        </div>
+        <div className="my-2">
+          <FontAwesomeIcon icon={faLinkedin} className="mr-2" />
+          <a href="https://www.linkedin.com/in/antoine-gagnon/">LinkedIn</a>
+        </div>
+        <div className="my-2">
+          <FontAwesomeIcon icon={faGithub} className="mr-2" />
+          <a href="https://github.com/antoinegag">@antoinegag</a>
+        </div>
+        <div className="my-2">
+          <FontAwesomeIcon icon={faTwitter} className="mr-2" />
+          <a href="https://twitter.com/antoinegag_dev">@antoinegag_dev</a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,14 @@
-import React, { Suspense } from "react";
+import React from "react";
 import PostList from "../../components/PostList";
 import { Link } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowAltCircleDown,
   faComments,
-  faMailBulk,
-  faEnvelope
+  faExclamationCircle,
 } from "@fortawesome/free-solid-svg-icons";
-import {
-  faTwitter,
-  faLinkedin,
-  faGithub
-} from "@fortawesome/free-brands-svg-icons";
 import { useTranslation } from "react-i18next";
+import Contact from "./Contact";
 
 export default function Home() {
   const { t, i18n } = useTranslation();
@@ -29,15 +24,24 @@ export default function Home() {
           />
         </div>
         <div className="w-full md:w-2/3 px-4 text-lg">
-          <h1 className="text-6xl font-bold tiktok">Antoine Gagnon.</h1>
+          <h1 className="text-4xl md:text-6xl font-bold tiktok">
+            Antoine Gagnon.
+          </h1>
           <p className="text-lg">{t("intro")}</p>
           <hr className="my-2 border-gray-500" />
           <p className="pb-5">
             {t("bio.school")}
             <br />
-            {t("bio.work")}
+            {/* {t("bio.work")} */}
           </p>
-          <p>
+          <p className="text-xl font-bold mb-2">
+            <p className="mb-2">
+              <FontAwesomeIcon
+                icon={faExclamationCircle}
+                className="text-blue-700 mr-2"
+              />
+              {t("bio.looking_internship")}
+            </p>
             <Link to="contact" spy={true} smooth={true} duration={600}>
               <FontAwesomeIcon icon={faComments} className="mr-2" />
               {t("contact.title")}
@@ -47,7 +51,9 @@ export default function Home() {
             {i18n.language === "fr" ? (
               <button onClick={() => i18n.changeLanguage("en")}>English</button>
             ) : (
-              <button onClick={() => i18n.changeLanguage("fr")}>Français</button>
+              <button onClick={() => i18n.changeLanguage("fr")}>
+                Français
+              </button>
             )}
           </div>
         </div>
@@ -72,30 +78,7 @@ export default function Home() {
         <PostList />
       </div>
       <div className="w-full second h-full" id="contact">
-        <div className="text-center py-10">
-          <h2 className="py-2">{t("contact.title")}.</h2>
-          <p className="text-lg">{t("contact.description")}</p>
-        </div>
-        <div className="text-center text-xl">
-          <div className="my-2">
-            <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
-            <a href="mailto:antoine.gagnon5@usherbrooke.ca" target="_top">
-              antoine.gagnon5@usherbrooke.ca
-            </a>
-          </div>
-          <div className="my-2">
-            <FontAwesomeIcon icon={faTwitter} className="mr-2" />
-            <a href="https://twitter.com/antoinegag_dev">@antoinegag_dev</a>
-          </div>
-          <div className="my-2">
-            <FontAwesomeIcon icon={faGithub} className="mr-2" />
-            <a href="https://github.com/antoinegag">@antoinegag</a>
-          </div>
-          <div className="my-2">
-            <FontAwesomeIcon icon={faLinkedin} className="mr-2" />
-            <a href="https://www.linkedin.com/in/antoine-gagnon/">LinkedIn</a>
-          </div>
-        </div>
+        <Contact />
       </div>
     </div>
   );
