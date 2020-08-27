361593fc72c9c3e86f396260ad7ca2504ba1b558
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-08-27 00:05:56 -0400

    Updates

---
+++ posts/curseforge-analytic-viewer.md
@@ -0,0 +1,4 @@
+## [Short demo (Click me!)](https://streamable.com/4xby3)
+
+
+## [Try it](https://antoinegag.github.io/curseforge-analytics-viewer/)
---
+++ precache-manifest.a03d4ccb7ef6c2b6da90ddd7ae204b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408d5830c6dd83f828d85f5588e686d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd57d8364c511bb41931",
+    "url": "/static/css/main.ba822be0.chunk.css"
+  },
+  {
+    "revision": "37291d0bad92d103ed58",
+    "url": "/static/js/2.6d316a0d.chunk.js"
+  },
+  {
+    "revision": "4679b3b23d18b28d5fc9f03891cf90f2",
+    "url": "/static/js/2.6d316a0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd57d8364c511bb41931",
+    "url": "/static/js/main.8abe07fb.chunk.js"
+  },
+  {
+    "revision": "7d8a31ab0a3d07555f2c",
+    "url": "/static/js/runtime-main.d78b460d.js"
+  }
+]);
