6d2cbc98a6cb8f9ab2646d48bedd4c8d2aad78d3
Antoine <antoine.gagnon760@gmail.com>
2020-12-06 21:16:33 -0500

    Remove old app

--- public/posts/welcome.md
+++
@@ -1,3 +0,0 @@
-# Welcome to antoineg.dev
-
-This is where I will post my projects as well as random tech related posts
--- src/hooks/usePost.js
+++
@@ -1,9 +0,0 @@
-import usePosts from "./usePosts";
-
-export default function usePost(slug, type) {
-  const { loading, posts } = usePosts(type);
-
-  if (loading) return { loading };
-
-  return { loading, post: posts[slug] };
-}
