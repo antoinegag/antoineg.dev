d04e304ee3205a04d4e6097b72bc98f9b82c6703
Antoine <antoine.gagnon760@gmail.com>
2020-12-06 21:16:28 -0500

    Add env

--- env-config.js
+++ env-config.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   "process.env.BACKEND_URL": prod ? "/" : "",
+  "process.env.GTAG_ID": process.env.GTAG_ID,
 };
