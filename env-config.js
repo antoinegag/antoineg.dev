8960904493651d0523f2dc1b2762fa64fb833580
Antoine <antoine.gagnon760@gmail.com>
2020-12-06 21:17:09 -0500

    Remove unused env

--- env-config.js
+++ env-config.js
@@ -1,8 +1,5 @@
-require("dotenv").config();
-
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   "process.env.BACKEND_URL": prod ? "/" : "",
-  "process.env.GTAG_ID": process.env.GTAG_ID,
 };
