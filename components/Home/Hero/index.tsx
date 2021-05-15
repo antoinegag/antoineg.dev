4c2392edbdc34df29fe3e90373c7476bb5482c10
Antoine Gagnon <antoine.gagnon760@gmail.com>
2021-05-15 16:52:51 -0400

    Update hero colors

--- components/Home/Hero/index.tsx
+++ components/Home/Hero/index.tsx
@@ -5,13 +5,13 @@ export default function Hero() {
   return (
     <div className="flex px-5 justify-center items-center min-h-screen bg-gradient-to-br to-matcha-50 from-peach-50 via-matcha-50">
       <div>
-        <h1 className="text-4xl md:text-6xl">
-          <span className="text-matcha-800 font-bold">Antoine</span>{" "}
+        <h1>
+          <span className="text-matcha-700 font-bold">Antoine</span>{" "}
           <span className="text-peach-500">Gagnon</span>
         </h1>
         <p className="text-peach-500 font-semibold text-xl">
           Curiosity & impact driven{" "}
-          <span className="font-bold text-matcha-800">Web Developer</span>
+          <span className="font-bold text-matcha-700">Web Developer</span>
         </p>
       </div>
       <div>
