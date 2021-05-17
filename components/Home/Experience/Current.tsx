19e441f563d45efd2e8e584bacc78d643c8e7f92
Antoine Gagnon <antoine.gagnon760@gmail.com>
2021-05-17 18:02:10 -0400

    Fix bottom padding on mission card

--- components/Home/Experience/Current.tsx
+++ components/Home/Experience/Current.tsx
@@ -10,7 +10,7 @@ const Current = (props: {
   console.log("in", inViewport);
 
   return (
-    <div className="overflow-hidden pt-32 min-h-screen flex flex-col justify-between px-5 bg-gradient-to-br to-green-50 from-peach-50 via-green-50">
+    <div className="overflow-hidden py-32 min-h-screen flex flex-col justify-between px-5 bg-gradient-to-br to-green-50 from-peach-50 via-green-50">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto" ref={forwardedRef}>
         <div
           className={classNames(
