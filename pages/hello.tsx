51d54061ca494efa19bfaf9ab370330e8f6ee924
Antoine Gagnon <antoine.gagnon760@gmail.com>
2022-06-12 10:59:41 -0400

    Preload next image on load

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -4,9 +4,29 @@ import Fade from "react-reveal/Fade";
 import { AppearingText } from "../components/Hello";
 
 export default function Hello() {
-  const getRandomImageIndex = () => Math.floor(Math.random() * 4);
+  const imageCount = 4;
+  const changeInterval = 4000;
+  const getRandomImageIndex = () => Math.floor(Math.random() * imageCount);
+
+  function getNextImageIndex(currentIndex: number) {
+    return (currentIndex + 1) % 4;
+  }
+
+  function getImageURL(index: number) {
+    return `/images/hello/${index}.jpg`;
+  }
+
+  function preLoadImage(index: number) {
+    const image = new Image();
+    image.src = getImageURL(getNextImageIndex(index));
+  }
+
   const [imageIndex, setImageIndex] = useState(getRandomImageIndex());
   const imageIndexRef = useRef(imageIndex);
+
+  useEffect(() => {
+    preLoadImage(imageIndex);
+  }, []);
 
   useEffect(() => {
     imageIndexRef.current = imageIndex;
@@ -14,8 +34,8 @@ export default function Hello() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setImageIndex((imageIndexRef.current + 1) % 4);
-    }, 5000);
+      setImageIndex(getNextImageIndex(imageIndexRef.current));
+    }, 4000);
 
     return () => clearInterval(interval);
   }, []);
@@ -26,6 +46,7 @@ export default function Hello() {
     "bg-gradient-to-b to-blue-100 from-blue-100 via-stone-200",
     "bg-gradient-to-b to-stone-100 from-blue-400 via-green-200",
   ];
+
   return (
     <div
       className={`min-h-full flex items-center justify-between flex-col font-semibold ${styles[imageIndex]}`}
@@ -41,10 +62,7 @@ export default function Hello() {
       </div>
       <div className="flex flex-col items-center justify-center">
         <div className="flex flex-col items-center mb-2">
-          <img
-            src={`/images/hello/${imageIndex}.jpg`}
-            className="rounded-full h-44"
-          />
+          <img src={getImageURL(imageIndex)} className="rounded-full h-44" />
         </div>
         <Fade top duration={750}>
           <div className="text-2xl mb-2 bg-gradient-to-tl text-white from-yellow-300 via-red-400 to-indigo-400 px-4 py-2 rounded-full">
